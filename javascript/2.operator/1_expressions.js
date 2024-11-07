82c1f9bff32625f445ff3b41bc32a5fb92d4fcae
DOJIN <djsong33@gmail.com>
2024-11-07 12:44:57 +0900

    js 모듈러 연산

---
+++ javascript/2.operator/1_expressions.js
@@ -0,0 +1,20 @@
+//선언문, 표현식
+/*
+1;  //숫자 리터럴 // ;<-한줄을 다차지함
+'hong'; //문자 리터럴 표현식
+1+1; //산술연산 표현식
+함수명(); => call(); //함수 표현식, 출력을 하거나 할당
+c = 100; // 할당, 할당표현식
+let a; //변수 선언문
+*/
+
+
+let a; //선언문 //a를 변수로 let 선언한다
+let b = 100; //표현식
+let c = undefined; //선언문
+let d = null; //선언문
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ javascript/6_const.js
@@ -0,0 +1,16 @@
+//const 상수 선언, 재할당 불가
+let a = 100;
+a = 10; //정수형
+a = 10.234; //실수형 변수
+a = true; //불린형
+a = 'Javascript';
+
+console.log(a);
+
+const MAX_VALUE = 100; // 상수- 변하지 않고 고정된 값
+// aa = 200;  //const는 바꾸려고하면 에러남
+// aa = 'CSS';
+
+console.log(MAX_VALUE);
+
+
