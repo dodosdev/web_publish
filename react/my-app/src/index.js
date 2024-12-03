39c6d310bdf674e84795d62473d16573bfdd3c00
DOJIN <djsong33@gmail.com>
2024-12-03 09:13:18 +0900

    index.js 업데이트

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -15,3 +15,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// React DOM 설명
+// https://ko.legacy.reactjs.org/docs/rendering-elements.html
