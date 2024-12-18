ca16c347254665ee51f05fdd98ee4504a8852a39
DOJIN <djsong33@gmail.com>
2024-12-18 17:53:07 +0900

    appbest

--- react/basic-state-app02/src/component/yes24/AppBestSeller.jsx
+++ react/basic-state-app02/src/component/yes24/AppBestSeller.jsx
@@ -30,9 +30,11 @@ export default function AppBestSeller() {
 
         }, [category]); 
         // }, [category]) 메뉴클릭할때마다 재호출됨. 
-        //의존성 배열(dependency array)은 특정 값들의 변화를 감지하여 Hook의 동작을 제어하는 역할을 한다. 주로 useEffect, useMemo, useCallback 등의 Hook에서 사용된다. 의존성 배열은 React에게 "이 값들이 변경될 때만 이 효과나 계산을 다시 실행해야 해"라고 알려주는 일종의 지시자 역할을 한다. 
+        //의존성 배열(dependency array)은 특정 값들의 변화를 감지하여 Hook의 동작을 제어하는 역할을 한다. 
+        // 주로 useEffect, useMemo, useCallback 등의 Hook에서 사용된다. 의존성 배열은 React에게 
+        // "이 값들이 변경될 때만 이 효과나 계산을 다시 실행해야 해"라고 알려주는 일종의 지시자 역할을 한다. 
 
-        
+
     // AppBestSeller <-- MenuList <-- Menu
     const handMenuClickReq2 = (category) => {
         console.log(`AppBestSeller ---> ${category}`); //탭메뉴클릭시 결과값이console에 출력되는부분
@@ -43,6 +45,7 @@ export default function AppBestSeller() {
     
     return (
         <div className='container'>
+            <h1>yes24 - Best books</h1>
             <MenuList list={menuList} click={handMenuClickReq2} />
             <BestBook list={bookList} />
         </div>
