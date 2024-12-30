066b761497d1d05ea8c7e070c75c3b2bdbd9b353
DOJIN <djsong33@gmail.com>
2024-12-30 18:18:29 +0900

    MYSQL 연산자

--- mysql/mysql84_실습코드.sql
+++ mysql/mysql84_실습코드.sql
@@ -62,10 +62,12 @@ SHOW TABLES;
 SELECT EMP_ID, EMP_NAME, GENDER, HIRE_DATE
 	FROM EMPLOYEE; 
 
+
 -- 사원테이블에서 사원명, 부서명, 입사일, 폰번호, 연봉조회
 DESC EMPLOYEE;
 SELECT EMP_NAME, DEPT_ID, PHONE, SALARY
 	FROM EMPLOYEE;
+    
     
 -- 부서테이블의 모든 칼럼을 조회
 SELECT * FROM DEPARTMENT;
@@ -137,6 +139,7 @@ SELECT *
 	FROM EMPLOYEE
 		WHERE EMP_NAME = '정주고';
 	
+    
 -- 사원테이블에서 사원아이디가 S0011 인 사원의 정보를 모두 조회
 SELECT *
 	FROM EMPLOYEE
@@ -147,6 +150,7 @@ SELECT *
 SELECT *
 	FROM EMPLOYEE
 		WHERE SALARY = 5800;
+    
     
 -- 사원테이블에서 여성사원들의 아이디, 이름, 입사일, 이메일 정보를 조회  
 SELECT EMP_ID, EMP_NAME, HIRE_DATE, EMAIL
@@ -159,12 +163,14 @@ SELECT EMP_ID '사원번호', EMP_NAME, HIRE_DATE
 	FROM EMPLOYEE
     WHERE DEPT_ID = 'SYS';
 
+
 -- WHERE절 조건에 별칭으로 조회가 가능할까요??
 SELECT EMP_ID '사원번호', EMP_NAME, HIRE_DATE, DEPT_ID '부서 번호'
 	FROM EMPLOYEE
 		WHERE DEPT_ID = 'SYS';
 		--  WHERE '부서번호' = 'SYS';
-     
+ 
+ 
 -- 사원테이블에서 마케팅 부서의 모든 사원 정보를 조회
 SELECT *
 	FROM EMPLOYEE
@@ -262,17 +268,22 @@ SELECT EMP_ID, EMP_NAME, HIRE_DATE, SALARY
 	FROM EMPLOYEE
     ORDER BY EMP_ID DESC;
     
+    
 -- 사원아이디 기준 오름차순, 입사일 기준 내림차순
 SELECT EMP_ID, EMP_NAME, HIRE_DATE, SALARY
 	FROM EMPLOYEE
     ORDER BY EMP_ID ASC, HIRE_DATE DESC;
+    
+    
     
 -- 급여를 기준으로 오름차순 정렬 후 조회
 SELECT *
 	FROM EMPLOYEE
     ORDER BY SALARY ASC;
     
+    
 -- SELECT DATABASE(); 데이터가있는지확인
+
 
 -- ENG_NANE이 정해지지 않은 사원들을 최근 입사한 순서대로 조회
 SELECT *
@@ -360,9 +371,12 @@ SELECT *
 **/
 -- 2016년 입사자들을 조회
 -- (2016-01-01 ~ 2016-12-31)
+
 SELECT *
 	FROM EMPLOYEE
 	WHERE HIRE_DATE BETWEEN '2016-01-01' AND '2016-12-31';  -- BETWEEN 비교연산으로 바꿔줌
+    
+    
 
     
 -- 사원아이디가 S0001, S0010, S0020 인 사원의 모든 정보를 조회
@@ -401,6 +415,7 @@ SELECT *
 /**
 	와일드 카드 문자 : 특정 문자열 검색 + LIKE
     종류 : %(전체), _(한문자)
+    
     사용법 : LIKE 연산자와 함께 조건식을 추가하여 사용됨
 	형식 - SELECT [컬럼리스트]
 			FROM [테이블명]
@@ -436,13 +451,191 @@ SELECT *
 	FROM EMPLOYEE
     WHERE EMP_NAME LIKE '%삼%';   -- 검색어 관련리스트 출력
     
+    
+/****************************************************
+		내장함수(Built-in) : 숫자, 문자, 날짜 함수
+        - 함수 테스트를 위한 테이블 DUAL
+        - SELECT [함수 실행] FROM DUAL;
+*****************************************************/
+-- 1. 숫자 함수 : abs(), rand(), truc()...
+-- (1) abs 함수 : 절대값 표현 함수
+SELECT 100, -100, ABS(100), ABS(-100)
+	FROM DUAL;
+    
+    
+-- (2) FLOOR 함수 : 소수점을 버리는(삭제) 함수
+-- 		TRUNC(TRUNCATE) : 소수점을 삭제하는 함수 - TURUNATE(데이터, 자릿수)
+
+SELECT 123.456, FLOOR(123.456) FROM DUAL;
+SELECT 123.456, 
+		TRUNCATE(123.456, 0)AS '소수점0',
+        TRUNCATE(123.456, 2) AS '소수점2'
+	FROM DUAL;
+    
+-- (3) RAND 함수 : 임의의 수를 생성
+SELECT RAND(),
+		RAND() * 1000,
+        RAND() * 100000
+		FROM DUAL;
+        
+-- 정수만 출력하는 쿼리 실행
+SELECT FLOOR(RAND())AS 'RAND1',
+		TRUNCATE (RAND() * 1000, 0) RAND2,
+        TRUNCATE (RAND() * 100000, 0)RAND3,
+        TRUNCATE (RAND() * 100000, 2) RAND4
+	FROM DUAL;    
+    
+-- (4) MOD 함수 : 모듈러 연산을 실행하는 함수 - MOD(숫자데이터, 연산숫자)
+SELECT MOD(100, 2) 짝수, MOD(101, 2) 홀수 FROM DUAL;
 
 
+-- 1~3 자리의 정수를 생성하고, 생성한 정수를 2로 나누는 모듈러 함수를 실행하는 쿼리를 완성해주세요.
+SELECT MOD(TRUNCATE (RAND() * 1000, 0), 2) AS RESULT
+	FROM DUAL;
 
 
+-- 사원테이블에서 사원아이디, 사원명, 부서아이디, 입사일, 연봉, 인센티브(연봉 20%)를 조회
+-- 인센티브의 소수점 생략
+-- 연봉협상이 아직 진행되지 않은 사원은 모두 0으로 출력, 인센티브 포함
+-- 
+SELECT EMP_ID,
+		EMP_NAME,
+        DEPT_ID,
+        HIRE_DATE,
+        IFNULL(SALARY, 0) AS SALARY,
+        IFNULL(TRUNCATE(SALARY * 0.2, 0), 0) AS INCENTIVE 
+	FROM EMPLOYEE
+    WHERE SALARY < 5000 OR SALARY IS NULL;
+    
+    
+    -- 2. 문자 함수 : CONCAT(), SUBSTRING()...
+    -- (1) CONCAT(문자열, 문자열...) : 문자열 결합
+    SELECT CONCAT('MY', 'SQL', '-84') AS NAME
+		FROM DUAL;
+        
+	-- 사원테이블의 사원명과 영어이름을 결합하여 새로운 컬럼을 생성하고 컬럼명은 TEST_NAME으로 조회
+    -- 예시) 홍길동/HONG
+    -- 영어이름이 정해지지 않은 사원은 빈문자열로 치환해서 실행
+    SELECT EMP_NAME, 
+			ENG_NAME,
+            CONCAT(EMP_NAME,  '/', IFNULL(ENG_NAME, '')) AS TEST_NAME  -- IFNULL문자열이 비어있으면..
+		FROM EMPLOYEE;
+        
+-- 사원테이블의 사원아이디와 1~5자리 사이의 임의의 정수를 결합하여 사원번호라는 새로운 컬럼을
+-- 생성하고 조회
+-- 사원아이디, 사원번호, 사원명, 입사일, 급여, 퇴사일 컬럼리스트를 조회
+-- 현재근무중인 사원은 현재 날짜를 출력
+SELECT EMP_ID,
+		CONAT(EMP_ID, '_', TRUNCATE(RAND() * 100000,0)) EMP_NUMBER,
+        EMP_NAME,
+        HIRE_DATE,
+        SALARY,
+        IFNULL(RETIRE_DATE, CURDATE()) RETIRE_DATE,
+        IFNULL(RETIRE_DATE, NOW()) RETIRE_DATE  -- NOW()시분초까지 다나옴
+        FROM EMPLOYEE;
+        
+-- (2) SUBSTRING(문자열, 위치, 추출자릿수) : 문자열 추출 함수
+SELECT 	SUBSTRING('대한민국 홍길동 만세 1234', 1, 4) 대한민국,
+		SUBSTRING('대한민국 홍길동 만세 1234', 6, 3) 홍길동,
+		SUBSTRING('대한민국 홍길동 만세 1234', 10, 2) 만세,
+		SUBSTRING('대한민국 홍길동 만세 1234', 17, 2) '!!'
+	FROM DUAL;
+    
+
+-- 사원테이블에서 사원아이디, 사원명, 입사년도, 입사월, 입사일, 급여를 조회
+SELECT HIRE_DATE FROM EMPLOYEE;
+SELECT EMP_ID,
+		EMP_NAME,
+        SUBSTRING(HIRE_DATE, 1, 4) YEAR,
+        SUBSTRING(HIRE_DATE, 6, 2) MONTH,
+        SUBSTRING(HIRE_DATE, 9, 2) DAY
+	FROM EMPLOYEE;
+    
+    
+-- 2015년도 입사한 모든 사원들을 조회
+SELECT *
+	FROM EMPLOYEE
+    WHERE SUBSTRING(HIR_DATE, 1, 4) = '2015';
+    
+SELECT HIRE_DATE FROM EMPLOYEE;
+
+-- 2018년도에 정보시스템 부서에 입사한 모든 사원들을 조회
+SELECT *
+	FROM EMPLOYEE
+    WHERE SUBSTRING(HIRE_DATE, 1, 4) = '2018'
+		AND DEPT_ID = 'SYS';
+		
+-- (3) LEFT(문자열, 추출숫자), RIGHT(문자열, 추출숫자)
+SELECT LEFT('대한민국 홍길동 만세 1234', 4) 대한민국,
+		RIGHY('대한민국 홍길동 만세 1234', 2) '!!'
+        FROM DUAL;
+        
+-- 2015년도에 입사함 모든 사원들을 조회
+SELECT *
+		FROM EMPLOYEE
+		WHERE LEFT(HIRE_DATE, 4) = '2015';
+        
+-- 사원들의 폰번호 마지막 4자리를 조회
+
+
+-- 사원명, 부서아이디, 입사년도, 폰번호(마지막 4자리) 조회
+SELECT EMP_NAME,
+		DEPT_ID,
+        LEFT(HIRE_DATE, 4) HIRE_DATE,
+        RIGHT(PHONE, 4) PHONE
+	FROM EMPLOYEE;
+    
+    
+-- (4) UPPER(대문자), LOWER(소문자)
+SELECT * FROM EMPLOYEE
+	WHERE UPPER(DEPT_ID) = UPPER('sys');
     
 
 
+-- 사원들의 영어이름과 이메일 주소를 모두 대문자로 조회
+SELECT EMP_ID,
+		EMP_NAME,
+        LOWER(ENG_NAME) ENG_NAME,
+        LOWER(EMAIL) EMAIL
+	FROM EMPLOYEE;
 
+
+-- (5) TRIM() : 공백 제거
+SELECT TRIM('       MYSQL 84') AS TRIM1,
+		TRIM('MYSQL 84       ') AS TRIM2,
+        TRIM('    MYSQL84      ') AS TRIM3
+    FROM DUAL;    
     
     
+-- (6) FORMAT(문자열, 소수점자리) : 문자열의 포맷 수정
+-- 숫자를 3자리씩 콤마로 구분하여 출력하는 포맷 생성
+SELECT FORMAT(123456, 0) FORMAT1,
+		FORMAT(123456, 2) FORMAT2 FROM DUAL;
+
+
+-- 사원테이블의 사원아이디, 사원명, 입사일, 연봉을 조회
+-- 연봉 협상 전인 사원은 0으로 변환 후 조회
+-- 연봉은 3자리씩 콤마로 구분하여 출력
+SELECT EMP_ID,
+		EMP_NAME,
+		HIRE_DATE,
+        CONCAT(FORMAT(IFNULL(SALARY, 0), 0) , '만원') SALARY
+	FROM EMPLOYEE;
+    
+
+
+-- 사원아이디, 사원명, 부서아이디, 입사일, 연봉, 보너스(연봉의 0.05%) 컬럼들을 조회
+-- 연봉과 보너스 컬럼은 3자리 콤마로 구분하여 출력 후 '만원' 추가
+-- 보너스 컬럼은 소수점 1자리까지 출력
+SELECT EMP_ID,
+		EMP_NAME,
+        DEPT_ID,
+        HIRE_DATE,
+		FORMAT(IFNULL(SALARY, 0), 0) SALARY,
+        FORMAT(IFNULL(SALARY * 0.05, 0), 1) AS BONUS
+	FROM EMPLOYEE;
+        
+    
+    
+    
+	
