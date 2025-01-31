22e4bea75581e6e9971110671143cdde1bd5e1c7
DOJIN <djsong33@gmail.com>
2025-01-31 17:48:40 +0900

    test router

--- shoppy/server/router/testRouter.js
+++ shoppy/server/router/testRouter.js
@@ -4,7 +4,7 @@ import * as controller from '../controller/testController.js';
 const router = express.Router();
 
 /** router 정보 매핑 */
-router.get('/', controller.getTest);  //  /test //대표이룸은 자동으로앞에붙음
+router.get('/', controller.getTest); // /test //대표이룸은 자동으로앞에붙음
 router.get('/product', controller.getTestProduct);  //  /test/product 
 
 export default router;
