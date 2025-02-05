17229124d48ea9c63543b920ff36220420eb11f3
dojin <djsong33@gmail.com>
2025-02-05 11:53:58 +0900

    member

--- shoppy/server/controller/memberController.js
+++ shoppy/server/controller/memberController.js
@@ -12,34 +12,8 @@ export const checkLogin = async(req, res) => {
 
 
 
-    // if(result === 1) {  // 로그인 성공 : jwt 토큰 생성 ⭕ 
-    //     jwt_token = jwt.sign({userId : req.body.id}, '5KldLlOVja');
-    //     // console.log('token-->> ', jwt_token);        
-    // } 
-    
-    // res.json({"result":result, "token":jwt_token});
-    // res.end();
 }
 
 
-/**
- * 아이디 중복체크 : getIdCheck
- */
-export const getIdCheck = async(req, res) => {
-    const result = await repository.getIdCheck(req.body);
-    res.json(result);
-    res.end();
-}
-
-
-/**
- * 회원가입 : registerMember
- */
-export const registerMember = async(req, res) => {
-    const formData = req.body;
-    const result = await repository.registerMember(formData);
-    res.json(result);
-    res.end();
-}
 
 
